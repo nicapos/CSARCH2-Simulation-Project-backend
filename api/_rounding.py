219b658ff3f0079452e9b201fa4db38b4cebfd80
nicapos <annikaposadas22@gmail.com>
2023-04-15 20:26:05 +0800

    add skeleton code for API

--- api/_rounding.py
+++ api/_rounding.py
@@ -12,3 +12,8 @@ def is_valid_rounding_method(option: str) -> bool:
 def round(value: int, rounding_method: RoundingMethod) -> int:
     # TODO: implement rounding
 	return 0
+
+def normalize(significand: int, exponent: int, rounding_method: RoundingMethod) -> tuple:
+	# TODO: return normalized and rounded value
+	# return normalized significand and adjusted exponent
+	return significand, exponent
--- api/index.py
+++ api/index.py
@@ -1,4 +1,8 @@
-from flask import Flask
+from flask import Flask, request
+from api._rounding import normalize, is_valid_rounding_method
+from api._convert import convert_bin, format_bin
+
+import json
 
 app = Flask(__name__)
 
@@ -8,9 +12,36 @@ def home():
 
 @app.route('/api/convert')
 def converter():
-    pass
     args = request.args
+
+    significand = args.get('significand', default=0, type=int)
+    exponent = args.get('exponent', default=0, type=int)
+
+    # TODO: Check for invalid arguments
+
+    repr_binary = convert_bin(significand, exponent)
+
+    repr_hex = hex(int(repr_binary, 2))[2:]
+    repr_binary = format_bin(repr_binary)
+
+    return {
+        'binary': repr_binary,
+        'hex': repr_hex
+    }
+
 @app.route('/api/normalize')
 def round():
-    pass
     args = request.args
+
+    param_significand = args.get('significand', default=0, type=int)
+    param_exponent = args.get('exponent', default=0, type=int)
+    rounding_method = args.get('rounding_method', default='truncate', type=str)
+
+    # TODO: Check for invalid arguments
+    
+    significand, exponent = normalize(param_significand, param_exponent, rounding_method)
+
+    return {
+        'significand': significand, 
+        'exponent': exponent
+    }
