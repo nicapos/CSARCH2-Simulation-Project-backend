5ce8510de3013fcaba03d0da2909f07b8db291eb
nicapos <annikaposadas22@gmail.com>
2023-04-16 21:10:34 +0800

    Update index.py
    apply rounding before normalization

--- api/index.py
+++ api/index.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from api._rounding import normalize, is_valid_rounding_method
+from api._rounding import round, normalize, is_valid_rounding_method
 from api._convert import convert_bin, format_bin
 import re
 
@@ -40,7 +40,8 @@ def round():
     param_exponent = args.get('exponent', default=0, type=int)
     rounding_method = args.get('rounding_method', default='truncate', type=str)
     
-    significand, exponent = normalize(param_significand, param_exponent, rounding_method)
+    rounded_significand = round(param_significand, rounding_method)
+    significand, exponent = normalize(rounded_significand, param_exponent, rounding_method)
 
     return {
         'significand': significand, 
